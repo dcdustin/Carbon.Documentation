e39400f1b8b75d752798fb4b29da11ac260ae77f
raul <raulssorban@gmail.com>
2025-04-03 00:20:55 +0200

    Deleted Carbon and Server submodules (I completed all endings)

--- tools/Generate.bat
+++ tools/Generate.bat
@@ -1,23 +1,85 @@
 @echo off
 
-SET root=%cd%\Server\win
-SET server=%root%\server
+if "%1" EQU "" (
+	set TAG=edge
+) else (
+	set TAG=%1
+)
 
-SET identity=generator
-SET name=Generator
-SET size=1000
-SET seed=1
-SET ip=localhost
+if "%TAG%" EQU "production" (
+	set BUILD=Release
+) else (
+	SET BUILD=Debug
+)
 
-rem Start up the server
+if "%2" EQU "" (
+	set BRANCH=public
+) else (
+	set BRANCH=%2
+)
+
+SET root=%cd%
+SET server=%root%\Server
+SET steam=%root%\Steam
+SET url=https://github.com/CarbonCommunity/Carbon/releases/download/%TAG%_build/Carbon.Windows.%BUILD%.zip	
+SET steamCmd=https://steamcdn-a.akamaihd.net/client/installer/steamcmd.zip
+
+echo Server directory: %server%
+echo Steam directory: %steam%
+echo Root directory: %root%
+echo Branch: %BRANCH%
+
+rem Ensure folders are created
+if not exist "%server%" mkdir "%server%"
+
+rem Download latest development build of Carbon
+echo Downloading Carbon from the '%TAG%' tag for %BUILD% build
+powershell -Command "(New-Object Net.WebClient).DownloadFile('%url%', '%root%\carbon.zip')"
+
+rem Extract it in the server folder
+cd %server%
+echo Extracting Carbon
+powershell -Command "Expand-Archive '%root%\carbon.zip' -DestinationPath '%server%'" -Force
+
+rem Download & extract Steam it in the steam folder
+if not exist "%steam%" (
+	mkdir "%steam%"
+	cd "%steam%"
+	
+	echo Downloading Steam
+	powershell -Command "(New-Object Net.WebClient).DownloadFile('%steamCmd%', '%root%\steam.zip')"
+	echo Extracting Steam
+	powershell -Command "Expand-Archive '%root%\steam.zip' -DestinationPath '%steam%'" -Force
+
+	del "%root%\steam.zip"
+)
+
+rem Cleanup
+del "%root%\carbon.zip"
+
+rem Download the server
+cd "%steam%"
+echo Downloading Rust server on %BRANCH% branch...
+steamcmd.exe +force_install_dir "%server%" ^
+			 +login anonymous ^
+             +app_update 258550 ^
+			 -beta %BRANCH% ^
+             validate ^
+             +quit ^
+		
 cd "%server%"
+echo Staring server...		
 RustDedicated.exe -nographics -batchmode -logs -silent-crashes ^
-                  -server.hostname "%name%" ^
-                  -server.identity "%identity%" ^
-                  -server.netlog ^
+                  -server.hostname "Generator" ^
+                  -server.identity "generator" ^
                   -server.saveinterval 400 ^
-                  -server.maxplayers 0 ^
-				  -carbon.scriptdir "..\..\..\Plugins" ^
-                  +server.seed %seed% ^
-                  +server.worldsize %size% ^
-                  -logfile "%identity%_log.txt" ^
+                  -server.maxplayers 1 ^
+                  -chat.serverlog 1 ^
+                  -global.asyncwarmup 1 ^
+                  -global.skipassetwarmup_crashes 1 ^
+                  -aimanager.nav_disable 1 ^
+                  +server.seed 123123 ^
+                  +server.worldsize 1500 ^
+                  -logfile "main_log.txt" ^
+			 
+exit /b 0
