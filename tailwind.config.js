e6beba2f125eb74b89cd99c3b6b9028ba7c31762
evs-ptr <evs.ptr@proton.me>
2025-05-07 19:32:59 +0500

    Exclude `cached` and `dist` from Tailwind parser

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './docs/.vitepress/**/*.{js,ts,vue}',
-    './docs/**/*.{js,ts,vue,md}'
+    './docs/**/*.{js,ts,vue,md}',
+    './docs/.vitepress/**/*.{js,ts,vue,md}',
+
+    '!./docs/.vitepress/cache',
+    '!./docs/.vitepress/dist'
   ],
   darkMode: 'selector',
   theme: {
