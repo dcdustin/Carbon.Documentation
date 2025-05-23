c5635c2ac38b8fc04d7dc864d0333199f0cad2e0
evs-ptr <evs.ptr@proton.me>
2025-05-23 20:15:25 +0500

    Prefabs - works
    - Search as was in Entities
    - Initialization of Minisearch hurts a little bit

---
+++ docs/.vitepress/components/PrefabsIndex.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { URL_METDAT_RUST_PREFABS } from '@/api/constants'
+import { Database, ExternalLink } from 'lucide-vue-next'
+import PrefabsViewer from './PrefabsViewer.vue'
+</script>
+
+<template>
+  <div class="max-w-screen-lg mx-auto px-4 py-8">
+    <div class="flex flex-col gap-4 mb-4">
+      <h1 class="text-2xl font-bold">Rust Prefab Reference</h1>
+      <p>
+        This section contains a comprehensive list of all prefabs available in the game. Each prefab is listed
+        with its unique ID, components, and file path.
+      </p>
+      <div class="flex items-center gap-2">
+        <a :href="URL_METDAT_RUST_PREFABS" target="_blank" class="flex items-center gap-2">
+          <Database :size="16" />
+          Prefabs API
+          <ExternalLink :size="14" class="opacity-80" />
+        </a>
+      </div>
+    </div>
+    <PrefabsViewer />
+  </div>
+</template>
+
+<style scoped>
+:deep(.VPBadge) {
+  margin-left: 0;
+  transform: none;
+}
+</style>
---
+++ docs/.vitepress/components/PrefabsViewer.vue
@@ -0,0 +1,171 @@
+<script setup lang="ts">
+import { fetchPrefabs, type Prefab } from '@/api/metadata/rust/prefabs'
+import AsyncState from '@/components/common/AsyncState.vue'
+import InfinitePageScroll from '@/components/common/InfinitePageScroll.vue'
+import SearchBar from '@/components/common/SearchBar.vue'
+import PrefabCard from '@/components/PrefabCard.vue'
+import { Search } from 'lucide-vue-next'
+import MiniSearch from 'minisearch'
+import { computed, onMounted, shallowRef } from 'vue'
+
+const isLoading = shallowRef(true)
+const error = shallowRef<string | null>(null)
+
+const prefabs = shallowRef<Prefab[]>([])
+const miniSearch = shallowRef<MiniSearch | null>(null)
+
+const debouncedSearchValue = shallowRef('')
+
+const pageSize = 20
+
+const filteredPrefabs = computed(() => {
+  if (!prefabs.value?.length) {
+    return []
+  }
+
+  if (!debouncedSearchValue.value) {
+    return prefabs.value
+  }
+
+  //   const startTime = performance.now()
+
+  let filtered = prefabs.value
+
+  const searchAsNumber = Number(debouncedSearchValue.value)
+  if (!isNaN(searchAsNumber) && searchAsNumber) {
+    const prefab = filtered.filter((prefab) => prefab.ID == searchAsNumber)
+    if (prefab.length > 0) {
+      return prefab
+    }
+  }
+
+  if (debouncedSearchValue.value && miniSearch.value) {
+    const results = miniSearch.value.search(debouncedSearchValue.value)
+    const prefabMap = new Map(filtered.map((prefab) => [prefab.ID, prefab]))
+    filtered = results.map((result) => prefabMap.get(result.ID)).filter(Boolean) as Prefab[]
+  }
+
+  //   const endTime = performance.now()
+  //   console.log(`Filtered items in ${endTime - startTime}ms - ${debouncedSearchValue.value}`)
+
+  return filtered
+})
+
+function tryLoadMiniSearch() {
+  const startTime = performance.now()
+
+  // should be extracted and cached...
+  miniSearch.value = new MiniSearch({
+    idField: 'ID',
+    fields: ['Name', 'Path', 'Components'],
+    storeFields: ['ID'],
+    searchOptions: {
+      prefix: true,
+      boost: {
+        Name: 4,
+        Components: 2,
+        Path: 1,
+      },
+      fuzzy: 0.1,
+    },
+    tokenize: (text, fieldName) => {
+      const SPACE_OR_PUNCTUATION = /[\n\r\p{Z}\p{P}_]+/u // from minisearch source + underscores
+      const processed = text
+        .replace(/([a-z0-9])([A-Z])/g, '$1 $2')
+        .replace(/([A-Z])([A-Z][a-z])/g, '$1 $2')
+        .toLowerCase()
+        .split(SPACE_OR_PUNCTUATION)
+        .filter((token) => token.length > 1)
+
+      processed.push(text.toLowerCase())
+
+      if (text && fieldName == 'Components') {
+        const split = text.split(',')
+        split.forEach((word) => {
+          processed.push(word.toLowerCase())
+        })
+
+        split
+          .map((mod) => mod.match(/[A-Z]/g)?.join(''))
+          .forEach((mod) => {
+            if (mod && mod.length > 1) {
+              processed.push(mod)
+            }
+          })
+      }
+      return [...new Set(processed)]
+    },
+  })
+
+  miniSearch.value.addAll(prefabs.value)
+
+  const endTime = performance.now()
+  console.log(`Initialized MiniSearch for Rust prefabs in ${endTime - startTime}ms`)
+}
+
+async function loadPrefabs() {
+  try {
+    isLoading.value = true
+    error.value = null
+
+    const data = await fetchPrefabs()
+
+    if (!data) {
+      throw new Error('No data received from API')
+    }
+
+    prefabs.value = data
+
+    tryLoadMiniSearch()
+  } catch (err) {
+    console.error('Failed to load prefabs:', err)
+    error.value = err instanceof Error ? err.message : 'Failed to load prefabs. Please try again later.'
+  } finally {
+    isLoading.value = false
+  }
+}
+
+onMounted(async () => {
+  loadPrefabs()
+})
+</script>
+
+<template>
+  <AsyncState :isLoading="isLoading" :error="error" loadingText="Loading prefabs...">
+    <SearchBar
+      v-model="debouncedSearchValue"
+      placeholder="Search prefabs..."
+      class="sticky min-[960px]:top-20 top-16 z-10"
+    >
+      <template #icon>
+        <Search class="text-gray-400" :size="20" />
+      </template>
+    </SearchBar>
+    <div v-if="filteredPrefabs && filteredPrefabs.length">
+      <div class="flex flex-col gap-6 mt-4">
+        <InfinitePageScroll :list="filteredPrefabs" :pageSize="pageSize" v-slot="{ renderedList }">
+          <div class="fixed bottom-4 sm:right-4 sm:left-auto left-1/2 z-10">
+            <div
+              class="text-sm text-gray-500 bg-zinc-100/40 dark:bg-gray-800/40 backdrop-blur-sm px-4 py-2 rounded-lg"
+            >
+              Rendering {{ renderedList.length }} of {{ filteredPrefabs.length }} filtered prefabs,
+              {{ prefabs.length }} total prefabs.
+            </div>
+          </div>
+          <div v-for="prefab in renderedList" :key="prefab.ID" :id="prefab.ID.toString()">
+            <PrefabCard :prefab="prefab" />
+          </div>
+        </InfinitePageScroll>
+      </div>
+    </div>
+    <div v-else class="py-8 flex flex-col items-center justify-center gap-2">
+      <p>No prefabs found matching your search</p>
+      <p v-if="prefabs && prefabs.length == 0" class="text-sm">
+        Debug: No prefabs loaded. Check console for errors.
+      </p>
+      <p v-else-if="debouncedSearchValue" class="text-sm">
+        Debug: Search query "{{ debouncedSearchValue }}" returned no results.
+      </p>
+    </div>
+  </AsyncState>
+</template>
