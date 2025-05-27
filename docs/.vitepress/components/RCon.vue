597b11efc6817648c948d1a4e8e2bf43edf5b632
raul <raulssorban@gmail.com>
2025-05-27 20:27:28 +0200

    Refactoring

--- docs/.vitepress/components/RCon.vue
+++ docs/.vitepress/components/RCon.vue
@@ -1,65 +1,66 @@
 <script lang="ts" setup>
 import { ref, onMounted } from 'vue'
 
-const address = ref('localhost:123')
-const password = ref('')
-
 const command = ref('')
-
 const logContainer = ref()
-const log = ref<string[]>([])
-const ws = ref<WebSocket | null>(null)
-const connected = ref(false)
 
-onMounted(() => {
-  log.value.push("<strong>Welcome to Carbon Documentation RCon</strong>")
-})
+class Server {
+  Address = 'localhost:28606'
+  Password = ''
+  Socket: WebSocket | null = null
+  Logs: string[] = []
+  IsConnected = false
 
-function connect() {
-  if (ws.value) {
-    ws.value.close()
+  connect() {
+    this.Socket?.close()
+
+    this.Socket = new WebSocket('ws://' + this.Address + '/' + this.Password)
+
+    this.Socket.onopen = () => {
+      this.IsConnected = true
+      this.Logs.push('Connected to ' + this.Address + ' successfully..')
+    }
+    this.Socket.onclose = () => {
+      this.IsConnected = false
+      this.Logs.push('Disconnected.')
+    }
+    this.Socket.onerror = (e) => {
+      this.Logs.push('Error: ' + e)
+    }
+    this.Socket.onmessage = (event) => {
+      const resp: CommandResponse = JSON.parse(event.data)
+      this.Logs.push(resp.Message)
+      command.value = command.value
+      logContainer.value.offsetHeight
+      logContainer.value.scrollTop = logContainer.value.scrollHeight
+    }
   }
 
-  ws.value = new WebSocket('ws://' + address.value + '/' + password.value)
+  sendCommand() {
+    if(!command.value) {
+      return;
+    }
 
-  ws.value.onopen = () => {
-    connected.value = true
-    log.value.push('Connected to ' + address.value + ' successfully..')
-  }
-  ws.value.onclose = () => {
-    connected.value = false
-    log.value.push('Disconnected.')
-  }
-  ws.value.onerror = (e) => {
-    log.value.push('Error: ' + e)
-  }
-  ws.value.onmessage = (event) => {
-    const resp: CommandResponse = JSON.parse(event.data)
-    log.value.push(resp.Message)
-    command.value = command.value
+    if (this.Socket && this.IsConnected) {
+      const packet: CommandSend = {
+        Name: 'RttstRs',
+        Message: command.value,
+        Identifier: 1,
+      }
+      this.Socket.send(JSON.stringify(packet))
+    }
+    this.Logs.push('<span style="color: var(--category-misc); user-select: none;"><strong>></strong></span> ' + command.value)
+    command.value = ''
     logContainer.value.offsetHeight
     logContainer.value.scrollTop = logContainer.value.scrollHeight
   }
 }
 
-function sendCommand() {
-  if(!command.value) {
-    return;
-  }
+const selectedServer = ref()
 
-  if (ws.value && connected.value) {
-    const packet: CommandSend = {
-      Name: 'RttstRs',
-      Message: command.value,
-      Identifier: 1,
-    }
-    ws.value.send(JSON.stringify(packet))
-  }
-  log.value.push('<span style="color: var(--category-misc); user-select: none;"><strong>></strong></span> ' + command.value)
-  command.value = ''
-  logContainer.value.offsetHeight
-  logContainer.value.scrollTop = logContainer.value.scrollHeight
-}
+onMounted(() => {
+  selectedServer.value.Logs.push("<strong>Welcome to Carbon Documentation RCon</strong>")
+})
 
 interface CommandSend {
   Name: string
@@ -88,14 +89,14 @@ enum LogType {
 <template>
   <div class="max-w-screen-lg mx-auto px-4 py-8 space-y-0">
     <div class="flex gap-2 items-center">
-      <input v-text="'test'" v-model="address" class="" placeholder="ws://host:port" />
-      <input v-model="password" class="" type="password" placeholder="RCON Password" />
-      <button class="p-2 ring-1 ring-white rounded-xl" @click="connect" :disabled="connected">Connect</button>
-      <span v-if="connected" class="text-green-600">Connected</span>
+      <input v-model="selectedServer.Address" v-if="selectedServer" class="" placeholder="Address" />
+      <input v-model="selectedServer.Password"v-if="selectedServer" class="" type="password" placeholder="RCON Password" />
+      <button class="p-2 ring-1 ring-white rounded-xl" @click="selectedServer.connect()" :disabled="selectedServer?.IsConnected">Connect</button>
+      <span v-if="selectedServer?.IsConnected" class="text-green-600">Connected</span>
     </div>
     <div style="height: 15px"></div>
     <div ref="logContainer" class="p-4 rounded text-sm font-mono " style="overflow: auto;  align-content: end; background-color: var(--vp-code-copy-code-bg); min-height: 300px; max-height: 700px; scrollbar-width: none;">
-      <p v-for="(line, i) in log" :key="i" v-html="line" style="white-space: pre-wrap; text-wrap-mode: nowrap;"></p>
+      <p v-for="(line, i) in selectedServer?.Logs" :key="i" v-html="line" style="white-space: pre-wrap; text-wrap-mode: nowrap;"></p>
     </div>
     <div class="flex gap-2" style="align-items: center; background-color: var(--vp-code-copy-code-bg); padding: 10px;">
       <div style="color: var(--category-misc); font-family: monospace; font-weight: 900; user-select: none;">> </div>
@@ -103,13 +104,11 @@ enum LogType {
         style="font-family: monospace; color: var(--docsearch-muted-color); width: -webkit-fill-available;"
         spellcheck="false"
         v-model="command"
-        @keyup.enter="sendCommand"
+        @keyup.enter="selectedServer?.sendCommand"
       />
-      <button @click="sendCommand" class="send-button"><span style="user-select: none">Send</span></button>
+      <button @click="selectedServer?.sendCommand" class="send-button"><span style="user-select: none">Send</span></button>
     </div>
   </div>
-
-
 </template>
 
 <style scoped>
