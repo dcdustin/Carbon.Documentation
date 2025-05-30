7b9725fede168f3b59c5558806a53a3397b86e61
evs-ptr <evs.ptr@proton.me>
2025-05-30 19:59:57 +0500

    `ServerBrowser` moar logic

---
+++ docs/.vitepress/components/ServerBrowserCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { Server } from '@/api/misc/server-list'
+
+const { server } = defineProps<{
+  server: Server
+}>()
+</script>
+
+<template>
+  <div class="flex flex-col gap-2">
+    <div class="flex flex-row gap-2">
+      <div class="flex flex-col gap-2">
+        <p>{{ server.hostname }}</p>
+        <p>{{ server.map }}</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped></style>
