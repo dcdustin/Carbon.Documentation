d490d7601e553176b017507d08e956d00982a4f7
evs-ptr <evs.ptr@proton.me>
2025-06-20 19:17:15 +0500

    `HooksIndex` remove note about state of the art (server browser is now)

--- docs/.vitepress/components/HooksIndex.vue
+++ docs/.vitepress/components/HooksIndex.vue
@@ -6,17 +6,15 @@ import HooksViewer from './HooksViewer.vue'
 </script>
 
 <template>
-  <div class="max-w-screen-lg mx-auto px-4 py-8">
-    <div class="flex flex-col gap-4 mb-4">
+  <div class="mx-auto max-w-screen-lg px-4 py-8">
+    <div class="mb-4 flex flex-col gap-4">
       <h1 class="text-2xl font-bold">Carbon Hooks Reference</h1>
       <p>
-        This section contains a comprehensive list of all hooks available in Carbon. Each hook is listed with
-        its name, category, and compatibility information.
+        This section contains a comprehensive list of all hooks available in Carbon. Each hook is listed with its name, category, and compatibility information.
       </p>
       <p>
-        By default, if hooks are not <VPBadge type="danger" text="Static" /> or
-        <VPBadge type="danger" text="Patch" />, they're dynamically applied upon plugin subscription,
-        otherwise inactive.
+        By default, if hooks are not <VPBadge type="danger" text="Static" /> or <VPBadge type="danger" text="Patch" />, they're dynamically applied upon plugin
+        subscription, otherwise inactive.
       </p>
       <div class="flex items-center gap-2">
         <a :href="URL_METDAT_CARB_HOOKS" target="_blank" class="flex items-center gap-2">
@@ -25,7 +23,6 @@ import HooksViewer from './HooksViewer.vue'
           <ExternalLink :size="14" class="opacity-80" />
         </a>
       </div>
-      <p class="text-sm text-gray-500 dark:text-gray-400">This page is state of the art - has some features that are not implemented in other API pages (yet)</p>
     </div>
     <HooksViewer />
   </div>
