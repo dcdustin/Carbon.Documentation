7a6eb5a456eaf48729a90778f6e99854a7495e6c
evs-ptr <evs.ptr@proton.me>
2025-06-28 18:31:36 +0500

    Extract api link

--- docs/.vitepress/components/ServerBrowserIndex.vue
+++ docs/.vitepress/components/ServerBrowserIndex.vue
@@ -1,8 +1,8 @@
 <script setup lang="ts">
 import { URL_SERVER_LIST } from '@/api/constants'
-import { Database, ExternalLink } from 'lucide-vue-next'
 import ServerBrowser from './ServerBrowser.vue'
 import ApiIndex from './common/ApiIndex.vue'
+import ApiLinkToApi from './common/ApiLinkToApi.vue'
 </script>
 
 <template>
@@ -10,13 +10,7 @@ import ApiIndex from './common/ApiIndex.vue'
     <div class="mb-4 flex flex-col gap-4">
       <h1 class="text-2xl font-bold">Carbon Server Browser</h1>
       <p>Browse and find Rust servers</p>
-      <div class="flex items-center gap-2">
-        <a :href="URL_SERVER_LIST" target="_blank" class="flex items-center gap-2">
-          <Database :size="16" />
-          Server List API
-          <ExternalLink :size="14" class="opacity-80" />
-        </a>
-      </div>
+      <ApiLinkToApi text="Server List API" :url="URL_SERVER_LIST" />
     </div>
     <ServerBrowser />
   </ApiIndex>
---
+++ docs/.vitepress/components/common/ApiLinkToApi.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import { Database, ExternalLink } from 'lucide-vue-next'
+
+const { text, url } = defineProps<{
+  text: string
+  url: string
+}>()
+</script>
+
+<template>
+  <div class="flex items-center gap-2">
+    <a :href="url" target="_blank" class="flex items-center gap-2">
+      <Database :size="16" />
+      {{ text }}
+      <ExternalLink :size="14" class="opacity-80" />
+    </a>
+  </div>
+</template>
