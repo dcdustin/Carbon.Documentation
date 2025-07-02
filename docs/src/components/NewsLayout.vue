e520b77f1807c8dc4f3bb9de858a7e4d7c7c0020
evs-ptr <evs.ptr@proton.me>
2025-07-02 16:34:44 +0500

    Better positioning. Wrap text. Bigger badges

--- docs/src/components/NewsLayout.vue
+++ docs/src/components/NewsLayout.vue
@@ -8,7 +8,7 @@ const { frontmatter } = useData()
 
 <template>
   <VPHomeContent>
-    <div v-if="frontmatter" class="fixed inset-0 overflow-hidden bg-neutral-950/100">
+    <div class="fixed inset-0 overflow-hidden bg-neutral-950/100">
       <div class="pointer-events-none fixed left-0 top-0 z-0 h-full w-full">
         <div class="relative h-[800px] w-full overflow-hidden opacity-60">
           <img :src="frontmatter.header" alt="Header background" class="news-hero absolute left-0 top-0 h-full w-full object-cover" />
@@ -17,13 +17,13 @@ const { frontmatter } = useData()
       </div>
     </div>
     <div class="relative">
-      <div class="mx-auto my-40 max-w-screen-lg space-y-6">
+      <div class="mx-auto my-40 flex max-w-screen-lg flex-col items-center gap-6 text-balance">
+        <img class="mx-auto w-[60%] transform transition-transform duration-200 hover:scale-105" :src="frontmatter.logo" />
+        <div class="my-3 flex flex-row gap-2 uppercase">
+          <VPBadge class="text-sm" type="info">{{ formatDate(frontmatter.date).string }}</VPBadge>
+          <VPBadge class="text-sm" v-for="tag in frontmatter.tags" :key="tag" type="tip">{{ tag }}</VPBadge>
+        </div>
         <div class="text-center text-5xl font-black uppercase" @click.stop>
-          <img class="mx-auto w-[60%] transform transition-transform duration-200 hover:scale-105" :src="frontmatter.logo" />
-          <div class="my-3 block">
-            <VPBadge type="info">{{ formatDate(frontmatter.date).string }}</VPBadge>
-            <VPBadge v-for="tag in frontmatter.tags" :key="tag" type="tip">{{ tag }}</VPBadge>
-          </div>
           {{ frontmatter.title }}
         </div>
         <div class="mb-48 text-center text-2xl font-normal text-slate-400" @click.stop>
