3b149c9da528709c1bc401162ceb7c71bd849b96
raul <raulssorban@gmail.com>
2025-07-05 15:15:30 +0200

    Centered title Fixed code blocks flipping out on mobile

--- docs/src/components/news/NewsSection.vue
+++ docs/src/components/news/NewsSection.vue
@@ -4,13 +4,10 @@
   </div>
 </template>
 
-<script setup lang="ts">
-
-</script>
-
 <style scoped>
 .news-section {
   padding: 25px !important;
   background-color: #131313d0;
+  contain: inline-size;
 }
 </style>
