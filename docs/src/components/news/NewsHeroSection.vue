4861f323e112f18bfbbbc35cd1e83626f6cee254
raul <raulssorban@gmail.com>
2025-07-15 17:08:24 +0200

    Touchups

--- docs/src/components/news/NewsHeroSection.vue
+++ docs/src/components/news/NewsHeroSection.vue
@@ -2,7 +2,7 @@
   <div class="news-section-hero">
     <div class="news-section-hero-media">
       <img :src="props.img" />
-      <div class="absolute bottom-0 left-0 h-full w-full bg-gradient-to-b from-neutral-950/35 to-neutral-950/100"></div>
+      <div class="absolute bottom-0 left-0 h-full w-full bg-gradient-to-b from-neutral-950/35 to-neutral-900/100"></div>
     </div>
     <div class="!mx-auto w-[80%] !max-w-screen-xl">
       <slot></slot>
@@ -24,6 +24,13 @@ const props = defineProps<Props>()
   margin-inline: calc(-1 * (100vw - 100%) / 2);
   padding-top: 0px;
   overflow: hidden;
+  filter: saturate(0%);
+  transition-property: all;
+  transition-duration: 75ms;
+}
+
+.news-section-hero:hover {
+  filter: saturate(150%);
 }
 
 .news-section-hero-media {
@@ -45,11 +52,5 @@ const props = defineProps<Props>()
   object-position: center;
   display: block;
   opacity: 100% !important;
-}
-
-.news-section-hero-section {
-  position: relative;
-  z-index: 1; /* ensure it appears above the image */
-  padding: 3rem 1rem;
 }
 </style>
