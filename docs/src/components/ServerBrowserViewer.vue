25f10c750089c512c319e42fbf7a3c782bc654bb
evs-ptr <evs.ptr@proton.me>
2025-08-08 16:32:02 +0500

    `ServerBrowserViewer` adjust search placeholder

--- docs/src/components/ServerBrowserViewer.vue
+++ docs/src/components/ServerBrowserViewer.vue
@@ -264,7 +264,7 @@ onMounted(async () => {
     <template #top>
       <SearchBar
         v-model="debouncedSearchValue"
-        placeholder="Search servers..."
+        placeholder="Search servers... (hostname, IP, map, raw tags)"
         class="sticky top-16 z-10 min-[960px]:top-20"
         :isExpandable="true"
         :initialExpanded="true"
